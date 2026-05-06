0317ba025c8b4a950fbe526ab42ad704e7fd2292
carloshazelcg <carloshazelcg@gmail.com>
2026-05-06 01:21:35 -0600

    Fix: Configuración.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare()
-});
+  adapter: cloudflare(),
 
-export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   }
 });
-
-
