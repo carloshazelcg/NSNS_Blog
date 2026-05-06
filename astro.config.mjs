def346348e0231a4040bd2a5c792e095dc72cceb
carloshazelcg <carloshazelcg@gmail.com>
2026-05-06 01:24:18 -0600

    Fix: Configuración V2.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,11 @@ import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare(),
-
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
   vite: {
     plugins: [tailwindcss()]
   }
