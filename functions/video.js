ecb47c1eee2acfd999c139262c0ef458e4762c3e
carloshazelcg <carloshazelcg@gmail.com>
2026-08-10 16:14:08 -0600

    Actualización: EL DUETO v3.

--- functions/video.js
+++ functions/video.js
@@ -1,7 +1,11 @@
 export async function onRequest(context) {
   const { env, request } = context;
   
-  const object = await env.MY_BUCKET.get('IMG_0340.MP4');
+  const rangeHeader = request.headers.get('range');
+  
+  const object = await env.MY_BUCKET.get('IMG_0340.MP4', {
+    range: rangeHeader ? rangeHeader : undefined,
+  });
 
   if (!object) {
     return new Response("Video no encontrado", { status: 404 });
@@ -12,7 +16,21 @@ export async function onRequest(context) {
   headers.set('etag', object.etag);
   headers.set('accept-ranges', 'bytes');
 
+  let status = 200;
+
+  if (object.range) {
+    status = 206;
+    headers.set(
+      'content-range',
+      `bytes ${object.range.offset}-${object.range.offset + object.range.length - 1}/${object.size}`
+    );
+    headers.set('content-length', object.range.length);
+  } else {
+    headers.set('content-length', object.size);
+  }
+
   return new Response(object.body, {
+    status,
     headers,
   });
 }
